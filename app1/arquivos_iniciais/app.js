6f88b915aa6f058d698c387e7b63b1b8bab317d4
Maria Eduarda Sales de Castro <castro.madusales@gmail.com>
2023-05-24 11:31:37 -0300

    Update app.js

--- app1/arquivos_iniciais/app.js
+++ app1/arquivos_iniciais/app.js
@@ -124,7 +124,7 @@ function carregaListaDespesas() {
     despesas = bd.recuperarTodosRegistros()
     
     //selecionando o elemento tbody da tabela
-    var listaDespesas = document.getElementById('listaDespesas')
+    let listaDespesas = document.getElementById('listaDespesas')
 
 
 /*
@@ -138,19 +138,30 @@ function carregaListaDespesas() {
 //percorrer o array despesas, listando cada despesa de forma dinâmica
 despesas.forEach(function(d) {
 
-    console.log(d)
+    //console.log(d)
     
     //criando a linha (tr)
     let linha = listaDespesas.insertRow()
 
     //criar as colunas (td)
     linha.insertCell(0).innerHTML = `${d.dia} ${d.mes} ${d.ano}`
-    linha.insertCell(1).innerHTML = d.tipo
+    
 
     //ajustar o tipo
-    switch(parseInt(d.tipo)) {
-        case 1
+    switch(d.tipo) {
+        case '1': d.tipo = 'Alimentação'
+            break
+        case '2': d.tipo = 'Educação'
+            break
+        case '3': d.tipo = 'Lazer'
+            break
+        case '4': d.tipo = 'Saúde'
+            break
+        case '5': d.tipo = 'Transporte'
+            break
     }
+
+    linha.insertCell(1).innerHTML = d.tipo
 
     linha.insertCell(2).innerHTML = d.descricao
     linha.insertCell(3).innerHTML = d.valor
