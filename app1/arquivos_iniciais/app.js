d993769eb530f7213db630c4c440539da34054c9
Maria Eduarda Sales de Castro <castro.madusales@gmail.com>
2023-05-26 09:34:08 -0300

    update

--- app1/arquivos_iniciais/app.js
+++ app1/arquivos_iniciais/app.js
@@ -67,6 +67,10 @@ class Bd {
 
     }
 
+    pesquisar(despesa) {
+        console.log(despesa)
+    }
+
 }
 
 let bd = new Bd()
@@ -178,12 +182,16 @@ despesas.forEach(function(d) {
 
 }
 
-function pesquisarDespesa( {
+function pesquisarDespesa() {
     let ano = document.getElementById('ano').value
     let mes = document.getElementById('mes').value
     let dia = document.getElementById('dia').value
     let tipo = document.getElementById('tipo').value
     let descricao = document.getElementById('descricao').value
     let valor = document.getElementById('valor').value
-})
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    
+}
 
